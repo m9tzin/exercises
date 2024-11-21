00eea95d26437dfe984e6920cc65a28cc48e6c01
m9tzin <matheusmrno@gmail.com>
2024-11-21 18:55:17 -0300

    new

---
+++ cpp/nextline.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	cout << "learning c++ and how to get nextline." << "\n";
+	cout << "my next" << "\n";
+	cout << "line" << "\n";
+	return 0;
+}
---
+++ cpp/numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	cout << 7;
+	cout << "\n";
+	cout << 7+3;
+	cout << "\n";
+	cout << (7+3)+(7+3);
+	cout << "\n";
+	return 0;
+}
